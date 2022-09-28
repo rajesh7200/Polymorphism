f83b68ce286c1d51c93b329f4ff7b082c73263ee
rajesh7200 <109009994+rajesh7200@users.noreply.github.com>
2022-09-28 21:06:22 +0530

    Update Polymorphism_1.java

--- Polymorphism_1.java
+++ Polymorphism_1.java
@@ -8,13 +8,17 @@ class Fruit{
 }
 
 class Apple extends Fruit{
-	void food
+	void apple1()
+	{
+		System.out.println("APPLE");
+	}
 }
 
 public class Polymorphism_1 {
 
 	public static void main(String[] args) {
-		
+		food();
+		apple1();
 
 	}
 
