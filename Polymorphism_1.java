b02e21ece20fac7f67dfebed9b5f69b649e9fa0a
rajesh7200 <109009994+rajesh7200@users.noreply.github.com>
2022-09-28 21:02:10 +0530

    Add files via upload

---
+++ Polymorphism_1.java
@@ -0,0 +1,21 @@
+package Polymorphism;
+
+class Fruit{
+	void food()
+	{
+		System.out.println("Fruit");
+	}
+}
+
+class Apple extends Fruit{
+	void food
+}
+
+public class Polymorphism_1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
